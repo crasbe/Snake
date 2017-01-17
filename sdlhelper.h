5032244d75b239ba2379a2b18ebef4eaa3228550
crasbe <crasbe@gmail.com>
2017-01-17 23:26:55 +0100

    sdlhelper.h adjusted for fillallsdl

--- sdlhelper.h
+++ sdlhelper.h
@@ -1,18 +1,20 @@
 #ifndef SDLHELPER_H_
 #define SDLHELPER_H_
 
+#include <stdint.h>
+
 #include "snake.h"
 
 // Return values of initsdl and addbmpsdl:
 // 0: everything went fine
 // 1: error
 
-int initsdl(struct properties* props);
-int addbmpsdl(struct properties* props, char *file,
-				unsigned int xpos, unsigned int ypos,
-				unsigned int xsize, unsigned int ysize);
-		
-void cleanupsdl(struct properties* props);
+extern int initsdl(struct properties* props);
+extern int addbmpsdl(struct properties* props, char *file,
+					unsigned int xpos, unsigned int ypos,
+					unsigned int xsize, unsigned int ysize);
+extern int fillallsdl(struct properties* props, uint32_t argb);
+extern void cleanupsdl(struct properties* props);
 
 
 #endif // SDLHELPER_H_
