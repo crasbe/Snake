e9901f9d0921d7caaaa11d46737c442fa24797ff
crasbe <crasbe@gmail.com>
2017-01-18 18:14:23 +0100

    menu_c updated

--- menu_c.c
+++ menu_c.c
@@ -2,10 +2,15 @@
 #include <stdint.h>
 
 #include "snake.h"
+#include "sdlhelper.h"
 
 int menu(struct properties* props) {
 	
 	printf("Hier ist die Menu-Funktion!\n");
+	
+	addbmpsdl(props, "img/snake.bmp", 0, 0, 256, 256);
+	
+	SDL_Delay(4000);
 	
 	return 1;
 }
